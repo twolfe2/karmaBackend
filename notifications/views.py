c5f2506417ceddd3ebe12a75391c92ddde482e60
Thomas Wolfe <tewolfe2@gmail.com>
2016-09-04 22:07:34 -0700

    send notification setup

--- notifications/views.py
+++ notifications/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from pyfcm import FCMNotification
 
+import os
+
+fcm_key = str(os.environ.get('FCM_KEY'))
+push_service = FCMNotification(api_key=fcm_key)
 # Create your views here.
 
 def index(request):
@@ -10,5 +15,12 @@ def index(request):
 
 def send(request):
   print request.method
-  print request.POST
-  return HttpResponse('hello from send')
+  if request.type == 'POST':
+    print request.POST
+    registration_id = request.POST['registration_id']
+    message_title = 'Application Accepted'
+    message_body = 'Hi, your application has been recieved'
+    result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+    print result
+    return HttpResponse('notification sent to'+registration_id)
+  return HttpResponse('Hello World')
